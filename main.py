3b72cf2dd2cf97a3d6cc9a1703b635e2b3ab8c99
deeplearning2020 <yeahia.ruet@gmail.com>
2025-01-10 21:04:45 +0600

    fixed typo

--- main.py
+++ main.py
@@ -246,21 +246,22 @@ class netG(nn.Module):
         super(netG, self).__init__()
         self.ReLU = nn.LeakyReLU(0.2, inplace=True)
         self.Tanh = nn.Tanh()
-        self.conv1 = nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False)
+        
+        # Starting from 1x1
+        self.conv1 = nn.ConvTranspose2d(nz, ngf * 8, 3, 1, 0, bias=False)  # 3x3
         self.BatchNorm1 = nn.BatchNorm2d(ngf * 8)
 
-        self.conv2 = nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False)
+        self.conv2 = nn.ConvTranspose2d(ngf * 8, ngf * 4, 3, 2, 1, bias=False)  # 5x5
         self.BatchNorm2 = nn.BatchNorm2d(ngf * 4)
-        #self.Drop2 = nn.Dropout2d(p=0.5)
         self.Drop2 = DropBlock2D()
 
-        self.conv3 = nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False)
+        self.conv3 = nn.ConvTranspose2d(ngf * 4, ngf * 2, 3, 1, 1, bias=False)  # 5x5
         self.BatchNorm3 = nn.BatchNorm2d(ngf * 2)
 
-        self.conv4 = nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False)
+        self.conv4 = nn.ConvTranspose2d(ngf * 2, ngf, 3, 2, 1, bias=False)  # 9x9
         self.BatchNorm4 = nn.BatchNorm2d(ngf)
 
-        self.conv6 = nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False)
+        self.conv6 = nn.ConvTranspose2d(ngf, nc, 3, 1, 1, bias=False)  # 11x11
 
         self.apply(weights_init)
 
@@ -281,6 +282,7 @@ class netG(nn.Module):
         x = self.conv4(x)
         x = self.BatchNorm4(x)
         x = self.ReLU(x)
+        
         x = self.conv6(x)
         output = self.Tanh(x)
         return output
@@ -291,26 +293,27 @@ class netD(nn.Module):
         super(netD, self).__init__()
         self.LeakyReLU = nn.LeakyReLU(0.2, inplace=True)
 
-        self.conv1 = nn.Conv2d(nc, ndf, 4, 2, 1, bias=False)
+        # Input: 11x11
+        self.conv1 = nn.Conv2d(nc, ndf, 3, 2, 1, bias=False)  # 6x6
         self.BatchNorm1 = nn.BatchNorm2d(ndf)
-        self.conv2 = nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False)
+        
+        self.conv2 = nn.Conv2d(ndf, ndf * 2, 3, 1, 1, bias=False)  # 6x6
         self.BatchNorm2 = nn.BatchNorm2d(ndf * 2)
-        #self.Drop2 = nn.Dropout2d(p=0.5)
         self.Drop2 = DropBlock2D()
-        self.conv3 = nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False)
+        
+        self.conv3 = nn.Conv2d(ndf * 2, ndf * 4, 3, 2, 1, bias=False)  # 3x3
         self.BatchNorm3 = nn.BatchNorm2d(ndf * 4)
-        self.conv4 = nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False)
+        
+        self.conv4 = nn.Conv2d(ndf * 4, ndf * 8, 3, 1, 0, bias=False)  # 1x1
         self.BatchNorm4 = nn.BatchNorm2d(ndf * 8)
-        self.conv5 = nn.Conv2d(ndf * 8, ndf * 2, 4, 1, 0, bias=False)
-        #self.disc_linear = nn.Linear(ndf * 2, 1)
+        
+        self.conv5 = nn.Conv2d(ndf * 8, ndf * 2, 1, 1, 0, bias=False)  # 1x1
         self.aux_linear = nn.Linear(ndf * 2, nb_label+1)
         self.softmax = nn.LogSoftmax(dim=-1)
-        #self.sigmoid = nn.Sigmoid()
         self.ndf = ndf
         self.apply(weights_init)
 
     def forward(self, input):
-
         x = self.conv1(input)
         x = self.LeakyReLU(x)
 
@@ -332,8 +335,6 @@ class netD(nn.Module):
         x = x.view(-1, self.ndf * 2)
         c = self.aux_linear(x)
         c = self.softmax(c)
-        #s = self.disc_linear(x).squeeze()
-        #s = self.sigmoid(s)
         return c
 
 
